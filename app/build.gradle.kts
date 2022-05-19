e58385ec5a7f2af2bce7f073d991212a707b43ad
Andrew Gunnerson <chillermillerlong@hotmail.com>
2022-05-18 21:47:15 -0400

    Version 1.1
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.chiller3.bcr"
         minSdk = 29
         targetSdk = 32
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
