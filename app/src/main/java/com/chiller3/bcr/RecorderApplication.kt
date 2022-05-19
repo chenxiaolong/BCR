e76035cbe8a296f366a809405170fb5349a59cae
Andrew Gunnerson <chillermillerlong@hotmail.com>
2022-05-18 21:41:22 -0400

    Target SDK 32 instead of the Tiramisu preview
    It turns out apps built with the preview SDK cannot be installed on
    older stable versions of Android.
    
    With BCR now targeting SDK 32, the creation of the notification channel
    has been moved from RecorderInCallService to RecorderApplication to
    ensure that Android 13 Beta devices automatically prompt for
    notification permissions on the first app launch.
    
    Once the stable SDK 33 is released, we can re-add the runtime
    permissions request for POST_NOTIFICATIONS.
    
    Fixes: #1
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- app/src/main/java/com/chiller3/bcr/RecorderApplication.kt
+++ app/src/main/java/com/chiller3/bcr/RecorderApplication.kt
@@ -1,13 +1,34 @@
 package com.chiller3.bcr
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import com.google.android.material.color.DynamicColors
 
 class RecorderApplication : Application() {
+    companion object {
+        const val CHANNEL_ID = "persistent"
+    }
+
     override fun onCreate() {
         super.onCreate()
 
         // Enable Material You colors
         DynamicColors.applyToActivitiesIfAvailable(this)
+
+        createNotificationChannel()
+    }
+
+    /**
+     * Create a low priority notification channel for the persistent notification.
+     */
+    private fun createNotificationChannel() {
+        val name: CharSequence = getString(R.string.notification_channel_persistent_name)
+        val description = getString(R.string.notification_channel_persistent_desc)
+        val channel = NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_LOW)
+        channel.description = description
+
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
     }
 }
