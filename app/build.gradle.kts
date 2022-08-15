866ff38284a59f5ca629818c52ddd9f0ad4352c5
Patryk Mis <patryk.mis@mykolab.com>
2022-08-15 16:06:37 +0200

    Force newer version of dependency of dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -148,6 +148,19 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.5.2")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("com.google.android.material:material:1.6.1")
+
+    // force newer version of dependency of dependency
+    implementation("androidx.annotation:annotation:1.4.0")
+
+    val collectionVersion = "1.2.0"
+    implementation("androidx.collection:collection:$collectionVersion")
+    implementation("androidx.collection:collection-ktx:$collectionVersion")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-process:2.5.1")
+    implementation("androidx.loader:loader:1.1.0")
+    implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
