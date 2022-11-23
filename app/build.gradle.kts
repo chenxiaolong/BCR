816f3115912477e4731f56d6b009cb82b656d3c4
Andrew Gunnerson <chillermillerlong@hotmail.com>
2022-11-23 00:19:14 -0500

    build.gradle.kts: Avoid breaking gradle's laziness where possible
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,6 @@
+import org.eclipse.jgit.api.ArchiveCommand
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.archive.TarFormat
 import org.eclipse.jgit.lib.ObjectId
 import org.jetbrains.kotlin.backend.common.pop
 import org.json.JSONObject
@@ -11,6 +13,7 @@ plugins {
 buildscript {
     dependencies {
         classpath("org.eclipse.jgit:org.eclipse.jgit:6.2.0.202206071550-r")
+        classpath("org.eclipse.jgit:org.eclipse.jgit.archive:6.2.0.202206071550-r")
         classpath("org.json:json:20220320")
     }
 }
@@ -93,6 +96,9 @@ val gitVersionName = getVersionName(git, gitVersionTriple)
 val projectUrl = "https://github.com/chenxiaolong/BCR"
 val releaseMetadataBranch = "master"
 
+val extraDir = File(buildDir, "extra")
+val archiveDir = File(extraDir, "archive")
+
 android {
     namespace = "com.chiller3.bcr"
 
@@ -110,6 +116,13 @@ android {
 
         buildConfigField("String", "PROJECT_URL_AT_COMMIT",
             "\"${projectUrl}/tree/${gitVersionTriple.third.name}\"")
+    }
+    sourceSets {
+        getByName("main") {
+            assets {
+                srcDir(archiveDir)
+            }
+        }
     }
     signingConfigs {
         create("release") {
@@ -154,11 +167,38 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
 
+val archive = tasks.register("archive") {
+    inputs.property("gitVersionTriple.third", gitVersionTriple.third)
+
+    val outputFile = File(archiveDir, "archive.tar")
+    outputs.file(outputFile)
+
+    doLast {
+        val format = "tar_${Thread.currentThread().id}"
+
+        ArchiveCommand.registerFormat(format, TarFormat())
+        try {
+            outputFile.outputStream().use {
+                git.archive()
+                    .setTree(git.repository.resolve(gitVersionTriple.third.name))
+                    .setFormat(format)
+                    .setOutputStream(it)
+                    .call()
+            }
+        } finally {
+            ArchiveCommand.unregisterFormat(format)
+        }
+    }
+}
+
 android.applicationVariants.all {
     val variant = this
     val capitalized = variant.name.capitalize()
-    val extraDir = File(buildDir, "extra")
     val variantDir = File(extraDir, variant.name)
+
+    variant.preBuildProvider.configure {
+        dependsOn(archive)
+    }
 
     val moduleProp = tasks.register("moduleProp${capitalized}") {
         inputs.property("projectUrl", projectUrl)
@@ -248,7 +288,8 @@ android.applicationVariants.all {
         inputs.property("variant.versionName", variant.versionName)
 
         archiveFileName.set("BCR-${variant.versionName}-${variant.name}.zip")
-        destinationDirectory.set(File(destinationDirectory.asFile.get(), variant.name))
+        // Force instantiation of old value or else this will cause infinite recursion
+        destinationDirectory.set(destinationDirectory.dir(variant.name).get())
 
         // Make the zip byte-for-byte reproducible (note that the APK is still not reproducible)
         isPreserveFileTimestamps = false
@@ -256,12 +297,12 @@ android.applicationVariants.all {
 
         dependsOn.add(variant.assembleProvider)
 
-        from(moduleProp.get().outputs)
-        from(addonD.get().outputs) {
+        from(moduleProp.map { it.outputs })
+        from(addonD.map { it.outputs }) {
             fileMode = 0b111_101_101 // 0o755; kotlin doesn't support octal literals
             into("system/addon.d")
         }
-        from(permissionsXml.get().outputs) {
+        from(permissionsXml.map { it.outputs }) {
             into("system/etc/permissions")
         }
         from(variant.outputs.map { it.outputFile }) {
