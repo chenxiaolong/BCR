chenxiaolong/BCR

commit 588d3f69b71a2b0691b0a0e29b397d32b7a8dd3f

Add support for customizing the output filename
This is a hidden feature and will not be exposed via BCR's GUI. A user
can customize the output filename by copying the default template to
<output directory>/bcr.properties and then editing the file.

Fixes: #187

Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

app/src/main/java/com/chiller3/bcr/RecorderThread.kt
@@ -67,6 +67,8 @@ class RecorderThread(
 
     // Filename
     private val filenameLock = Object()
+    private var pendingCallDetails: Call.Details? = null
+    private lateinit var filenameTemplate: FilenameTemplate
     private lateinit var filename: String
     private val redactions = HashMap<String, String>()
 
@@ -110,67 +112,83 @@ class RecorderThread(
      */
     fun onCallDetailsChanged(details: Call.Details) {
         synchronized(filenameLock) {
+            if (!this::filenameTemplate.isInitialized) {
+                // Thread hasn't started yet, so we haven't loaded the filename template
+                pendingCallDetails = details
+                return
+            }
+
             redactions.clear()
 
-            filename = buildString {
-                val instant = Instant.ofEpochMilli(details.creationTimeMillis)
-                callTimestamp = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())
-                append(FORMATTER.format(callTimestamp))
+            filename = filenameTemplate.evaluate {
+                when (it) {
+                    "date" -> {
+                        val instant = Instant.ofEpochMilli(details.creationTimeMillis)
+                        callTimestamp = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())
+                        return@evaluate FORMATTER.format(callTimestamp)
+                    }
+                    "direction" -> {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            when (details.callDirection) {
+                                Call.Details.DIRECTION_INCOMING -> return@evaluate "in"
+                                Call.Details.DIRECTION_OUTGOING -> return@evaluate "out"
+                                Call.Details.DIRECTION_UNKNOWN -> {}
+                            }
+                        }
+                    }
+                    "sim_slot" -> {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+                            && context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE)
+                            == PackageManager.PERMISSION_GRANTED
+                            && context.packageManager.hasSystemFeature(
+                                PackageManager.FEATURE_TELEPHONY_SUBSCRIPTION)) {
+                            val subscriptionManager = context.getSystemService(SubscriptionManager::class.java)
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    when (details.callDirection) {
-                        Call.Details.DIRECTION_INCOMING -> append("_in")
-                        Call.Details.DIRECTION_OUTGOING -> append("_out")
-                        Call.Details.DIRECTION_UNKNOWN -> {}
+                            // Only append SIM slot ID if the device has multiple active SIMs
+                            if (subscriptionManager.activeSubscriptionInfoCount > 1) {
+                                val telephonyManager = context.getSystemService(TelephonyManager::class.java)
+                                val subscriptionId = telephonyManager.getSubscriptionId(details.accountHandle)
+                                val subscriptionInfo = subscriptionManager.getActiveSubscriptionInfo(subscriptionId)
+
+                                return@evaluate "${subscriptionInfo.simSlotIndex + 1}"
+                            }
+                        }
+                    }
+                    "phone_number" -> {
+                        if (details.handle?.scheme == PhoneAccount.SCHEME_TEL) {
+                            redactions[details.handle.schemeSpecificPart] = "<phone number>"
+
+                            return@evaluate details.handle.schemeSpecificPart
+                        }
+                    }
+                    "caller_name" -> {
+                        val callerName = details.callerDisplayName?.trim()
+                        if (!callerName.isNullOrBlank()) {
+                            redactions[callerName] = "<caller name>"
+
+                            return@evaluate callerName
+                        }
+                    }
+                    "contact_name" -> {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            val contactName = details.contactDisplayName?.trim()
+                            if (!contactName.isNullOrBlank()) {
+                                redactions[contactName] = "<contact name>"
+
+                                return@evaluate contactName
+                            }
+                        }
+                    }
+                    else -> {
+                        Log.w(tag, "Unknown filename template variable: $it")
                     }
                 }
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
-                    && context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE)
-                        == PackageManager.PERMISSION_GRANTED
-                    && context.packageManager.hasSystemFeature(
-                        PackageManager.FEATURE_TELEPHONY_SUBSCRIPTION)) {
-                    val subscriptionManager = context.getSystemService(SubscriptionManager::class.java)
-
-                    // Only append SIM slot ID if the device has multiple active SIMs
-                    if (subscriptionManager.activeSubscriptionInfoCount > 1) {
-                        val telephonyManager = context.getSystemService(TelephonyManager::class.java)
-                        val subscriptionId = telephonyManager.getSubscriptionId(details.accountHandle)
-                        val subscriptionInfo = subscriptionManager.getActiveSubscriptionInfo(subscriptionId)
-
-                        append("_sim")
-                        append(subscriptionInfo.simSlotIndex + 1)
-                    }
-                }
-
-                if (details.handle?.scheme == PhoneAccount.SCHEME_TEL) {
-                    append('_')
-                    append(details.handle.schemeSpecificPart)
-
-                    redactions[details.handle.schemeSpecificPart] = "<phone number>"
-                }
-
-                val callerName = details.callerDisplayName?.trim()
-                if (!callerName.isNullOrBlank()) {
-                    append('_')
-                    append(callerName)
-
-                    redactions[callerName] = "<caller name>"
-                }
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    val contactName = details.contactDisplayName?.trim()
-                    if (!contactName.isNullOrBlank()) {
-                        append('_')
-                        append(contactName)
-
-                        redactions[contactName] = "<contact name>"
-                    }
-                }
+                null
             }
-            // AOSP's SAF automatically replaces invalid characters with underscores, but just
-            // in case an OEM fork breaks that, do the replacement ourselves to prevent
-            // directory traversal attacks.
+            // AOSP's SAF automatically replaces invalid characters with underscores, but just in
+            // case an OEM fork breaks that, do the replacement ourselves to prevent directory
+            // traversal attacks.
                 .replace('/', '_').trim()
 
             Log.i(tag, "Updated filename due to call details change: ${redact(filename)}")
@@ -190,7 +208,14 @@ class RecorderThread(
             if (isCancelled) {
                 Log.i(tag, "Recording cancelled before it began")
             } else {
-                val initialFilename = synchronized(filenameLock) { filename }
+                val initialFilename = synchronized(filenameLock) {
+                    filenameTemplate = FilenameTemplate.load(context)
+
+                    onCallDetailsChanged(pendingCallDetails!!)
+                    pendingCallDetails = null
+
+                    filename
+                }
                 val outputFile = createFileInDefaultDir(initialFilename, format.mimeTypeContainer)
                 resultUri = outputFile.uri
 
@@ -609,9 +634,19 @@ class RecorderThread(
                     return null
                 }
 
-                val second = name.indexOf('_', first + 1)
+                // The user might not have a delimiter in the template, but we know for sure the
+                // date ends at least 4 digits after the +/- in the date offset
+                var second = name.indexOfAny(charArrayOf('-', '+'), first + 1)
                 if (second < 0) {
                     return null
+                }
+                second += 5
+
+                // If there are two additional digits, then assume they are part of the offset
+                if (second + 2 <= name.length
+                    && name[second].isDigit()
+                    && name[second + 1].isDigit()) {
+                    second += 2
                 }
 
                 return ZonedDateTime.parse(name.substring(0, second), FORMATTER)

app/src/main/java/com/chiller3/bcr/FilenameTemplate.kt
@@ -1,0 +1,119 @@
+package com.chiller3.bcr
+
+import android.content.Context
+import android.util.Log
+import androidx.documentfile.provider.DocumentFile
+import java.util.*
+import java.util.regex.Pattern
+
+class FilenameTemplate private constructor(props: Properties) {
+    private val components = arrayListOf<Component>()
+
+    init {
+        Log.d(TAG, "Filename template: $props")
+
+        while (true) {
+            val index = components.size
+            val text = props.getProperty("filename.$index.text") ?: break
+            val default = props.getProperty("filename.$index.default")
+            val prefix = props.getProperty("filename.$index.prefix")
+            val suffix = props.getProperty("filename.$index.suffix")
+
+            components.add(Component(text, default, prefix, suffix))
+        }
+
+        if (components.isEmpty() || !components[0].text.startsWith(VAR_DATE)) {
+            throw IllegalArgumentException("The first filename component must begin with $VAR_DATE")
+        }
+
+        Log.d(TAG, "Loaded filename components: $components")
+    }
+
+    fun evaluate(getVar: (String) -> String?): String {
+        val varCache = hashMapOf<String, String?>()
+        val getVarCached = { name: String ->
+            varCache.getOrPut(name) {
+                getVar(name)
+            }
+        }
+
+        return buildString {
+            for (c in components) {
+                var text = evalVars(c.text, getVarCached)
+                if (text.isEmpty() && c.default != null) {
+                    text = evalVars(c.default, getVarCached)
+                }
+                if (text.isNotEmpty()) {
+                    if (c.prefix != null) {
+                        append(evalVars(c.prefix, getVarCached))
+                    }
+                    append(text)
+                    if (c.suffix != null) {
+                        append(evalVars(c.suffix, getVarCached))
+                    }
+                }
+            }
+        }
+    }
+
+    private data class Component(
+        val text: String,
+        val default: String?,
+        val prefix: String?,
+        val suffix: String?,
+    )
+
+    companion object {
+        private val TAG = FilenameTemplate::class.java.simpleName
+
+        private val VAR_PATTERN = Pattern.compile("""\${'$'}\{(\w+)\}""")
+        private val VAR_DATE = "${'$'}{date}"
+
+        private fun evalVars(input: String, getVar: (String) -> String?): String =
+            StringBuffer().run {
+                val m = VAR_PATTERN.matcher(input)
+
+                while (m.find()) {
+                    val name = m.group(1)!!
+                    val replacement = getVar(name)
+
+                    m.appendReplacement(this, replacement ?: "")
+                }
+
+                m.appendTail(this)
+
+                toString()
+            }
+
+        fun load(context: Context): FilenameTemplate {
+            val props = Properties()
+
+            val prefs = Preferences(context)
+            val outputDir = prefs.outputDir?.let {
+                // Only returns null on API <21
+                DocumentFile.fromTreeUri(context, it)!!
+            } ?: DocumentFile.fromFile(prefs.defaultOutputDir)
+
+            val templateFile = outputDir.findFile("bcr.properties")
+            if (templateFile != null) {
+                try {
+                    Log.d(TAG, "Loading custom filename template: ${templateFile.uri}")
+
+                    context.contentResolver.openInputStream(templateFile.uri)?.use {
+                        props.load(it)
+                        return FilenameTemplate(props)
+                    }
+                } catch (e: Exception) {
+                    Log.w(TAG, "Failed to load custom filename template", e)
+                }
+            }
+
+            Log.d(TAG, "Loading default filename template")
+
+            context.resources.openRawResource(R.raw.filename_template).use {
+                props.load(it)
+                return FilenameTemplate(props)
+            }
+        }
+    }
+}

app/src/main/res/raw/filename_template.properties
@@ -1,0 +1,69 @@
+# This file specifies the filename template for BCR's output files. To change
+# the default filename template, copy this file to `bcr.properties` in the
+# output directory and edit it to your liking.
+#
+# Syntax/rules:
+# 1. The filename components start at 0.
+# 2. Do not skip numbers for the components. For example, if there are 4
+#    components: 0, 1, 2, 4, then 4 is ignored because there's a gap in the
+#    middle.
+# 3. Blank lines and lines beginning with # are ignored.
+# 4. The file extension is not part of this template. File extensions are
+#    automatically determined by Android.
+#
+# Available options:
+# - filename.<num>.text: The text to add to the filename. Variables are included
+#   with the ${...} syntax. If a variable is not defined, then it is replaced
+#   with an empty string.
+# - filename.<num>.default: If `text` is empty, then this value is used as a
+#   fallback.
+# - filename.<num>.prefix: If `text` (and `default`) are not empty, then this
+#   value is added to the beginning.
+# - filename.<num>.suffix: If `text` (and `default`) are not empty, then this
+#   value is added to the end.
+#
+# Troubleshooting:
+#   If there is a syntax error, BCR will ignore the custom template and fall
+#   back to the default. To find out more details, enable debug mode by long
+#   pressing BCR's version number. After the next phone call, BCR will create a
+#   log file in the output directory. Search for `FilenameTemplate` in the log
+#   file.
+
+# Time of call. Must always be the first component.
+filename.0.text = ${date}
+
+# Call direction, which is either `in` or `out`. Only defined on Android 10+.
+filename.1.text = ${direction}
+filename.1.prefix = _
+
+# SIM slot number. Only defined on Android 11+ if multiple SIMs are active and
+# the user has granted the phone permission.
+filename.2.text = ${sim_slot}
+filename.2.prefix = _sim
+
+# Phone number of the other party in the call.
+filename.3.text = ${phone_number}
+filename.3.prefix = _
+
+# Caller ID as provided by CNAP from the carrier.
+filename.4.text = ${caller_name}
+filename.4.prefix = _
+
+# Contact name. Only defined on Android 11+ if the user has granted the contacts
+# permission.
+filename.5.text = ${contact_name}
+filename.5.prefix = _
+
+################################################################################
+
+# Example: Add the call direction to the filename with a leading underscore. If
+# the call direction can't be determined, then add "unknown" instead.
+#filename.<num>.text = ${direction}
+#filename.<num>.default = unknown
+#filename.<num>.prefix = _
+
+# Example: Add the contact name to the filename if it exists. Otherwise, fall
+# back to the caller ID.
+#filename.<num>.text = ${contact_name}
+#filename.<num>.default = ${caller_name}
+#filename.<num>.prefix = _

commit 466b7c7acaf3b97699ee70acbbb694ceb3736a3a

RecorderThread: Fix silent crash when debug mode is enabled
The output filename variable was not initialized at the point that
startLogcat() was called, resulting in an uninitialized property
exception being thrown. This bug was introduced in commit
588d3f69b71a2b0691b0a0e29b397d32b7a8dd3f.

This commit also fixes startLogcat() accessing the filename property
without acquiring the filenameLock lock. In practice, this did not cause
issues, but better to be correct.

Discovered in issue #195.

Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

app/src/main/java/com/chiller3/bcr/RecorderThread.kt
@@ -200,6 +200,13 @@ class RecorderThread(
         var errorMsg: String? = null
         var resultUri: Uri? = null
 
+        synchronized(filenameLock) {
+            filenameTemplate = FilenameTemplate.load(context)
+
+            onCallDetailsChanged(pendingCallDetails!!)
+            pendingCallDetails = null
+        }
+
         startLogcat()
 
         try {
@@ -208,14 +215,7 @@ class RecorderThread(
             if (isCancelled) {
                 Log.i(tag, "Recording cancelled before it began")
             } else {
-                val initialFilename = synchronized(filenameLock) {
-                    filenameTemplate = FilenameTemplate.load(context)
-
-                    onCallDetailsChanged(pendingCallDetails!!)
-                    pendingCallDetails = null
-
-                    filename
-                }
+                val initialFilename = synchronized(filenameLock) { filename }
                 val outputFile = createFileInDefaultDir(initialFilename, format.mimeTypeContainer)
                 resultUri = outputFile.uri
 
@@ -288,7 +288,9 @@ class RecorderThread(
 
         Log.d(tag, "Starting log file (${BuildConfig.VERSION_NAME})")
 
-        logcatFile = createFileInDefaultDir("${filename}.log", "text/plain")
+        val logFilename = synchronized(filenameLock) { "${filename}.log" }
+
+        logcatFile = createFileInDefaultDir(logFilename, "text/plain")
         logcatProcess = ProcessBuilder("logcat", "*:V")
             // This is better than -f because the logcat implementation calls fflush() when the
             // output stream is stdout. logcatFile is guaranteed to have file:// scheme because it's