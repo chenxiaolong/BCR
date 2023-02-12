3b0999ac1e3424047e44acf52efc2e17f7257415
Andrew Gunnerson <accounts+github@chiller3.com>
2023-02-11 19:39:29 -0500

    Update all dependencies
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -160,15 +160,15 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-ktx:1.6.1")
-    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("androidx.fragment:fragment-ktx:1.5.5")
     implementation("androidx.preference:preference-ktx:1.2.0")
-    implementation("com.google.android.material:material:1.7.0")
+    implementation("com.google.android.material:material:1.8.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 val archive = tasks.register("archive") {
