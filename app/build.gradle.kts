63f0f816863d674422b98f50da062162db929583
Patryk Mis <24607131+PatrickMis@users.noreply.github.com>
2023-03-23 14:18:20 +0100

    Bump AndroidX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -175,11 +175,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.6.1")
+    implementation("androidx.activity:activity-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.documentfile:documentfile:1.0.1")
-    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation("androidx.fragment:fragment-ktx:1.5.6")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("com.google.android.material:material:1.8.0")
     testImplementation("junit:junit:4.13.2")
