chenxiaolong/BCR

commit de0bd10c49e58f5ab6e9c7a0948a79bd1d2e3d5e

Contact.kt: Fix Cursor resource leak
Same fix as for DocumentFileExtensions.kt in
5620fbb49893cefcbb0cea5d91a46d071a6a1229.

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/src/main/java/com/chiller3/bcr/output/CallMetadataCollector.kt
@@ -16,7 +16,7 @@ import android.telephony.TelephonyManager
 import android.util.Log
 import androidx.core.database.getStringOrNull
 import com.chiller3.bcr.extension.phoneNumber
-import com.chiller3.bcr.findContactsByPhoneNumber
+import com.chiller3.bcr.withContactsByPhoneNumber
 import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -60,13 +60,14 @@ class CallMetadataCollector(
 
         Log.d(TAG, "Performing manual contact lookup")
 
-        for (contact in findContactsByPhoneNumber(context, number)) {
-            Log.d(TAG, "Found contact display name via manual lookup")
-            return contact.displayName
+        val contact = withContactsByPhoneNumber(context, number) { it.firstOrNull() }
+        if (contact == null) {
+            Log.d(TAG, "Contact not found via manual lookup")
+            return null
         }
 
-        Log.d(TAG, "Contact not found via manual lookup")
-        return null
+        Log.d(TAG, "Found contact display name via manual lookup")
+        return contact.displayName
     }
 
     private fun getContactDisplayName(details: Call.Details, allowManualLookup: Boolean): String? {

app/src/main/java/com/chiller3/bcr/rule/RecordRule.kt
@@ -11,9 +11,9 @@ import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.util.Log
 import com.chiller3.bcr.GroupLookup
-import com.chiller3.bcr.findContactsByPhoneNumber
-import com.chiller3.bcr.getContactGroupMemberships
 import com.chiller3.bcr.output.PhoneNumber
+import com.chiller3.bcr.withContactGroupMemberships
+import com.chiller3.bcr.withContactsByPhoneNumber
 
 sealed class RecordRule {
     abstract val record: Boolean
@@ -177,10 +177,9 @@ sealed class RecordRule {
             if (contactsAllowed) {
                 contactLookupKeys = hashSetOf<String>().apply {
                     for (number in numbers) {
-                        findContactsByPhoneNumber(context, number)
-                            .asSequence()
-                            .map { it.lookupKey }
-                            .toCollection(this)
+                        withContactsByPhoneNumber(context, number) { contacts ->
+                            contacts.map { it.lookupKey }.toCollection(this)
+                        }
                     }
                 }
 
@@ -188,9 +187,9 @@ sealed class RecordRule {
                 if (rules.any { it is ContactGroup }) {
                     contactGroupIds = hashSetOf<GroupLookup>().apply {
                         for (lookupKey in contactLookupKeys) {
-                            getContactGroupMemberships(context, lookupKey)
-                                .asSequence()
-                                .toCollection(this)
+                            withContactGroupMemberships(context, lookupKey) {
+                                it.toCollection(this)
+                            }
                         }
                     }
                 }

app/src/main/java/com/chiller3/bcr/Contact.kt
@@ -13,8 +13,10 @@ import android.provider.ContactsContract
 import androidx.annotation.RequiresPermission
 import androidx.core.database.getLongOrNull
 import androidx.core.database.getStringOrNull
+import com.chiller3.bcr.extension.asSequence
 import com.chiller3.bcr.output.PhoneNumber
 import kotlinx.parcelize.Parcelize
+import java.io.IOException
 
 private val PROJECTION = arrayOf(
     ContactsContract.PhoneLookup.LOOKUP_KEY,
@@ -51,7 +53,11 @@ data class ContactGroupInfo(
 ) : Parcelable
 
 @RequiresPermission(Manifest.permission.READ_CONTACTS)
-fun findContactsByPhoneNumber(context: Context, number: PhoneNumber): Iterator<ContactInfo> {
+fun <R> withContactsByPhoneNumber(
+    context: Context,
+    number: PhoneNumber,
+    block: (Sequence<ContactInfo>) -> R,
+): R {
     val rawNumber = number.toString()
 
     // Same heuristic as InCallUI's PhoneNumberHelper.isUriNumber()
@@ -64,7 +70,7 @@ fun findContactsByPhoneNumber(context: Context, number: PhoneNumber): Iterator<C
             numberIsSip.toString())
         .build()
 
-    return findContactsByUri(context, uri)
+    return withContactsByUri(context, uri, block)
 }
 
 @RequiresPermission(Manifest.permission.READ_CONTACTS)
@@ -73,26 +79,29 @@ fun getContactByLookupKey(context: Context, lookupKey: String): ContactInfo? {
         .appendPath(lookupKey)
         .build()
 
-    return findContactsByUri(context, uri).asSequence().firstOrNull()
+    return withContactsByUri(context, uri) { it.firstOrNull() }
 }
 
-fun findContactsByUri(context: Context, uri: Uri) = iterator {
-    context.contentResolver.query(uri, PROJECTION, null, null, null)?.use { cursor ->
+fun <R> withContactsByUri(context: Context, uri: Uri, block: (Sequence<ContactInfo>) -> R): R {
+    val cursor = context.contentResolver.query(uri, PROJECTION, null, null, null)
+        ?: throw IOException("Query returned null cursor: $uri")
+
+    return cursor.use {
         val indexLookupKey = cursor.getColumnIndexOrThrow(ContactsContract.PhoneLookup.LOOKUP_KEY)
         val indexName = cursor.getColumnIndexOrThrow(ContactsContract.PhoneLookup.DISPLAY_NAME)
 
-        if (cursor.moveToFirst()) {
-            yield(ContactInfo(cursor.getString(indexLookupKey), cursor.getString(indexName)))
-
-            while (cursor.moveToNext()) {
-                yield(ContactInfo(cursor.getString(indexLookupKey), cursor.getString(indexName)))
-            }
-        }
+        block(cursor.asSequence().map {
+            ContactInfo(cursor.getString(indexLookupKey), cursor.getString(indexName))
+        })
     }
 }
 
 @RequiresPermission(Manifest.permission.READ_CONTACTS)
-fun getContactGroupMemberships(context: Context, lookupKey: String) = iterator {
+fun <R> withContactGroupMemberships(
+    context: Context,
+    lookupKey: String,
+    block: (Sequence<GroupLookup>) -> R,
+): R {
     val selection = buildString {
         append(ContactsContract.CommonDataKinds.GroupMembership.LOOKUP_KEY)
         append(" = ? AND ")
@@ -104,35 +113,24 @@ fun getContactGroupMemberships(context: Context, lookupKey: String) = iterator {
         ContactsContract.CommonDataKinds.GroupMembership.CONTENT_ITEM_TYPE,
     )
 
-    context.contentResolver.query(
+    val cursor = context.contentResolver.query(
         ContactsContract.Data.CONTENT_URI,
         PROJECTION_GROUP_MEMBERSHIP,
         selection,
         selectionArgs,
         null,
-    )?.let { cursor ->
+    ) ?: throw IOException("Query returned null cursor")
+
+    return cursor.use {
         val indexRowId = cursor.getColumnIndexOrThrow(
             ContactsContract.CommonDataKinds.GroupMembership.GROUP_ROW_ID)
         val indexSourceId = cursor.getColumnIndexOrThrow(
             ContactsContract.CommonDataKinds.GroupMembership.GROUP_SOURCE_ID)
 
-        if (cursor.moveToFirst()) {
-            cursor.getLongOrNull(indexRowId)?.let {
-                yield(GroupLookup.RowId(it))
-            }
-            cursor.getStringOrNull(indexSourceId)?.let {
-                yield(GroupLookup.SourceId(it))
-            }
-
-            while (cursor.moveToNext()) {
-                cursor.getLongOrNull(indexRowId)?.let {
-                    yield(GroupLookup.RowId(it))
-                }
-                cursor.getStringOrNull(indexSourceId)?.let {
-                    yield(GroupLookup.SourceId(it))
-                }
-            }
-        }
+        block(cursor.asSequence().mapNotNull {
+            cursor.getLongOrNull(indexRowId)?.let { GroupLookup.RowId(it) }
+                ?: cursor.getStringOrNull(indexSourceId)?.let { GroupLookup.SourceId(it) }
+        })
     }
 }
 
@@ -155,39 +153,34 @@ fun getContactGroupById(context: Context, id: GroupLookup): ContactGroupInfo? {
         }
     }
 
-    return findContactGroups(context, selection, selectionArgs).asSequence().firstOrNull()
+    return withContactGroups(context, selection, selectionArgs) { it.firstOrNull() }
 }
 
-fun findContactGroups(
+fun <R> withContactGroups(
     context: Context,
     selection: String? = null,
     selectionArgs: Array<String>? = null,
-) = iterator {
-    context.contentResolver.query(
+    block: (Sequence<ContactGroupInfo>) -> R,
+): R {
+    val cursor = context.contentResolver.query(
         ContactsContract.Groups.CONTENT_URI,
         CONTACT_GROUP_PROJECTION,
         selection,
         selectionArgs,
         null,
-    )?.use { cursor ->
+    ) ?: throw IOException("Query returned null cursor")
+
+    return cursor.use {
         val indexRowId = cursor.getColumnIndexOrThrow(ContactsContract.Groups._ID)
         val indexSourceId = cursor.getColumnIndexOrThrow(ContactsContract.Groups.SOURCE_ID)
         val indexTitle = cursor.getColumnIndexOrThrow(ContactsContract.Groups.TITLE)
 
-        if (cursor.moveToFirst()) {
-            yield(ContactGroupInfo(
+        block(cursor.asSequence().map {
+            ContactGroupInfo(
                 cursor.getLong(indexRowId),
                 cursor.getString(indexSourceId),
                 cursor.getString(indexTitle),
-            ))
-
-            while (cursor.moveToNext()) {
-                yield(ContactGroupInfo(
-                    cursor.getLong(indexRowId),
-                    cursor.getString(indexSourceId),
-                    cursor.getString(indexTitle),
-                ))
-            }
-        }
+            )
+        })
     }
 }

app/src/main/java/com/chiller3/bcr/rule/PickContactGroupViewModel.kt
@@ -10,7 +10,7 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.chiller3.bcr.ContactGroupInfo
-import com.chiller3.bcr.findContactGroups
+import com.chiller3.bcr.withContactGroups
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -30,18 +30,19 @@ class PickContactGroupViewModel(application: Application) : AndroidViewModel(app
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
                 val groups = try {
-                    findContactGroups(getApplication())
-                        .asSequence()
-                        .sortedWith { o1, o2 ->
-                            compareValuesBy(
-                                o1,
-                                o2,
-                                { it.title },
-                                { it.rowId },
-                                { it.sourceId },
-                            )
-                        }
-                        .toList()
+                    withContactGroups(getApplication()) { contactGroups ->
+                        contactGroups
+                            .sortedWith { o1, o2 ->
+                                compareValuesBy(
+                                    o1,
+                                    o2,
+                                    { it.title },
+                                    { it.rowId },
+                                    { it.sourceId },
+                                )
+                            }
+                            .toList()
+                    }
                 } catch (e: Exception) {
                     Log.w(TAG, "Failed to list all contact groups", e)
                     return@withContext

app/src/main/java/com/chiller3/bcr/rule/RecordRulesViewModel.kt
@@ -15,9 +15,9 @@ import androidx.lifecycle.viewModelScope
 import com.chiller3.bcr.ContactGroupInfo
 import com.chiller3.bcr.GroupLookup
 import com.chiller3.bcr.Preferences
-import com.chiller3.bcr.findContactsByUri
 import com.chiller3.bcr.getContactByLookupKey
 import com.chiller3.bcr.getContactGroupById
+import com.chiller3.bcr.withContactsByUri
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -211,7 +211,7 @@ class RecordRulesViewModel(application: Application) : AndroidViewModel(applicat
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
                 val contact = try {
-                    findContactsByUri(getApplication(), uri).asSequence().firstOrNull()
+                    withContactsByUri(getApplication(), uri) { it.firstOrNull() }
                 } catch (e: Exception) {
                     Log.w(TAG, "Failed to query contact at $uri", e)
                     return@withContext

commit 17b985000991837465f33b449e59cb4aec2a04c0

Fix crash when contact query fails in direct boot mode
This regressed after de0bd10c49e58f5ab6e9c7a0948a79bd1d2e3d5e, which
turned null Cursor return values from ContentResolver queries into
exceptions.

This updates the CallMetadataCollector (and RecordRule) queries to catch
exceptions when querying contacts.

Fixes: #643

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/src/main/java/com/chiller3/bcr/output/CallMetadataCollector.kt
@@ -60,7 +60,12 @@ class CallMetadataCollector(
 
         Log.d(TAG, "Performing manual contact lookup")
 
-        val contact = withContactsByPhoneNumber(context, number) { it.firstOrNull() }
+        val contact = try {
+            withContactsByPhoneNumber(context, number) { it.firstOrNull() }
+        } catch (e: Exception) {
+            Log.d(TAG, "Failed to look up contact", e)
+            return null
+        }
         if (contact == null) {
             Log.d(TAG, "Contact not found via manual lookup")
             return null

app/src/main/java/com/chiller3/bcr/rule/RecordRule.kt
@@ -5,10 +5,8 @@
 
 package com.chiller3.bcr.rule
 
-import android.Manifest
 import android.content.Context
 import android.content.SharedPreferences
-import android.content.pm.PackageManager
 import android.os.Parcelable
 import android.util.Log
 import com.chiller3.bcr.GroupLookup
@@ -169,12 +167,10 @@ data class RecordRule(
             direction: CallDirection?,
             simSlot: Int?,
         ): Action {
-            val contactsAllowed = context.checkSelfPermission(Manifest.permission.READ_CONTACTS) ==
-                    PackageManager.PERMISSION_GRANTED
             var contactLookupKeys = emptySet<String>()
             var contactGroupIds = emptySet<GroupLookup>()
 
-            if (contactsAllowed) {
+            try {
                 contactLookupKeys = hashSetOf<String>().apply {
                     for (number in numbers) {
                         withContactsByPhoneNumber(context, number) { contacts ->
@@ -193,8 +189,10 @@ data class RecordRule(
                         }
                     }
                 }
-            } else {
-                Log.i(TAG, "Contacts permission not granted")
+            } catch (e: SecurityException) {
+                Log.w(TAG, "Permission denied when querying contacts and contact groups", e)
+            } catch (e: Exception) {
+                Log.w(TAG, "Failed to query contacts and contact groups", e)
             }
 
             for (rule in rules) {