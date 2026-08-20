09794c697ee5609e21913c11bed2f7fad1d19183
Andrew Gunnerson <accounts+github@chiller3.com>
2026-08-20 10:09:58 -0400

    gradle: Remove redundant File object construction
    The File constructor normalizes slashes in the child path component
    anyway.
    
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -540,7 +540,7 @@ fun updateChangelog(version: String?, replaceFirst: Boolean) {
 }
 
 fun updateMagiskChangelog(gitRef: String) {
-    File(File(File(File(projectDir, "magisk"), "updates"), "release"), "changelog.txt")
+    File(projectDir, "magisk/updates/release/changelog.txt")
         .writeText("The changelog can be found at: [`CHANGELOG.md`]($projectUrl/blob/$gitRef/CHANGELOG.md).\n")
 }
 
